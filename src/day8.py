e522e2bb6033dc6603bb4fe3bc4bf509dcd2f162
Michael Grossfeld <pantaryl@gmail.com>
2018-12-08 00:59:35 -0500

    Cleanup and better code reuse for Day8

--- src/day8.py
+++ src/day8.py
@@ -1,60 +1,46 @@
-import sys
 with open("../input/day8.txt", 'r') as inputFile:
     data = inputFile.read()
 data = data.split(" ")
 
-# Part 1
-def parseChild(myList):
-    mySum = 0
-    numChildren = int(myList.pop(0))
-    numMetadata = int(myList.pop(0))
-
-    if numChildren > 0:
-        for i in range(numChildren):
-            addedmySum, newList = parseChild(myList)
-            mySum += addedmySum
-            myList = newList
-    for i in range(numMetadata):
-        value = int(myList.pop(0))
-        mySum += value
-    return mySum, myList
-
-part1Sum, _ = parseChild(data.copy())
-print(part1Sum)
-
-
-# Part 2
 class Node():
     def __init__(self):
         self.children = []
         self.metadata = []
 
-def parseChild2(myList, parent):
+def parseChild(parent):
     newNode = Node()
     if parent is not None:
         parent.children.append(newNode)
 
-    numChildren = int(myList.pop(0))
-    numMetadata = int(myList.pop(0))
+    numChildren = int(data.pop(0))
+    numMetadata = int(data.pop(0))
 
     if numChildren > 0:
         for i in range(numChildren):
-            myList, _ = parseChild2(myList, newNode)
+            parseChild(newNode)
     for i in range(numMetadata):
-        value = int(myList.pop(0))
+        value = int(data.pop(0))
         newNode.metadata.append(value)
-    return myList, newNode
+    return newNode
 
-def traverseTree(node):
+root = parseChild(None)
+
+# Part 1
+def traversePart1(node):
+    mySum  = sum([traversePart1(child) for child in node.children])
+    mySum += sum(node.metadata)
+    return mySum
+print(traversePart1(root))
+
+# Part 2
+def traversePart2(node):
     mySum = 0
     if len(node.children) > 0:
         for i in range(len(node.metadata)):
             childIdx = node.metadata[i] - 1
             if childIdx < len(node.children):
-                mySum += traverseTree(node.children[childIdx])
+                mySum += traversePart2(node.children[childIdx])
     else:
         mySum += sum(node.metadata)
     return mySum
-
-_, root = parseChild2(data.copy(), None)
-print(traverseTree(root))
+print(traversePart2(root))
